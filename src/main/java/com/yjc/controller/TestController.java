2c1104c8da58c7577db0ac88121991ba630a6dfd
yangjiacheng <249118773@qq.com>
2020-06-24 10:40:10 +0800

    commit

---
+++ src/main/java/com/yjc/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.yjc.controller;
+
+/**
+ * @author jiacheng Yang
+ * @create 2020/6/24
+ **/
+public class TestController {
+    public static void main(String[] args) {
+        System.out.println("主函数");
+    }
+
+    private void test(){
+        System.out.println("私有方法");
+    }
+}
