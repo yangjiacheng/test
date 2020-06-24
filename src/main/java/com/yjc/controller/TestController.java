4fa93ab4d507ac3c25e5acf4685dddc6d8705b0b
yangjiacheng <249118773@qq.com>
2020-06-24 10:41:02 +0800

    second commit

--- src/main/java/com/yjc/controller/TestController.java
+++ src/main/java/com/yjc/controller/TestController.java
@@ -8,4 +8,8 @@ public class TestController {
     public static void main(String[] args) {
         System.out.println("主函数");
     }
+
+    private void test(){
+        System.out.println("私有方法");
+    }
 }
